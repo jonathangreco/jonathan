6c16b56f35adb9a5f53a13dc93bdefdeef515924
jonathangreco <nataniel.greco@gmail.com>
2014-12-03 23:03:19 +0100

    update

--- module/Admin/src/Admin/Service/RightsService.php
+++ module/Admin/src/Admin/Service/RightsService.php
@@ -7,6 +7,7 @@
 namespace Admin\Service;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Application\Entity\HierarchicalRole;
 
 class RightsService
 {
@@ -24,10 +25,32 @@ class RightsService
     }
 
     /**
+     * On récupère un role précis
+     * @param  integer $id
+     * @return HierarchicalRole instance
+     */
+    public function getRole($id)
+    {
+        return $this->em->getRepository('Application\Entity\HierarchicalRole')->find($id);
+    }
+
+    /**
      * On récupère toutes nos permissions
      */
     public function getPermissions()
     {
         return $this->em->getRepository('Application\Entity\Permission')->findAll();
+    }
+
+    public function addRole(HierarchicalRole $role)
+    {
+        $this->em->persist($role);
+        $this->em->flush($role);
+    }
+
+    public function deleteRole(HierarchicalRole $role)
+    {
+        $this->em->remove($role);
+        $this->em->flush($role);
     }
 }
--- module/Application/src/Application/Repository/HierarchicalRoleRepository.php
+++ module/Application/src/Application/Repository/HierarchicalRoleRepository.php
@@ -6,8 +6,17 @@
 namespace Application\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Application\Entity\HierarchicalRole;
 
 class HierarchicalRoleRepository extends EntityRepository
 {
-	
+	/**
+	 * Permet de récupérer les permissions associé à un role
+	 * @param  HierarchicalRole $role
+	 * @return array de Permissions
+	 */
+	public function getPermissionForRole(HierarchicalRole $role)
+	{
+
+	}
 }
